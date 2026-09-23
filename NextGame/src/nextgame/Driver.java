c36864c1c607dcfee124a9ed671119144b11a910
Clinton Stovall <cstova01@uafs.edu>
2026-09-23 10:56:01 -0500

    Wednesday work

--- NextGame/src/nextgame/Driver.java
+++ NextGame/src/nextgame/Driver.java
@@ -1,12 +1,22 @@
 package nextgame;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Driver {
 
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader (new FileReader ("notWordleHighscore.txt"));
+		Scanner scan = new Scanner (System.in);
+		String name;
+		
+		System.out.println("What is Your Name");
+		name = scan.nextLine();
+		
+		NextGame nextgame = new NextGame();
+		
+		nextgame.randomize();
 		
 		
 		
